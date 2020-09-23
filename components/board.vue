0d76760cd12e7523a64acd8fe467b4187bb92044
立石真由 <tateishimayu@tateishimayunoMacBook-puro.local>
2020-09-23 19:25:58 +0900

    0923

--- components/board.vue
+++ components/board.vue
@@ -1,7 +1,7 @@
 <template>
 <g>
 <polygon :points="points" fill="gray"></polygon>
-<cell v-for="p in coords" :a="p.a" :b="p.b" exp="50"></cell>
+<cell v-for="p in coords()" :a="p.a" :b="p.b" exp="50"></cell>
 <piece v-for="(p,i) in conf" :a="p.a" :b="p.b" :n="p.n" :index="i"
        :r="neighbors()" exp="50" @puton="puton"></piece>
 </g>
@@ -11,38 +11,33 @@
 import cell from '~/components/slot.vue'
 import piece from '~/components/piece.vue'
 export default{
-    props: ['level','exp'],
+    props: ['level','exp','problem'],
     data(){
 	return {
-	    coords:[],
-	    conf:[{a:0,b:1,n:2},
-		  {a:1,b:0,n:2},//
-		  {a:-1,b:1,n:2},//
-		  {a:1,b:-1,n:2},
-		  {a:-1,b:0,n:2},
-		  {a:0,b:-1,n:2},
-		  {a:-2,b:0,n:2},
-		  {a:-2,b:2,n:2},
-		  {a:0,b:2,n:2},//
-		  {a:2,b:0,n:2},
-		  {a:2,b:-2,n:2},
-		  {a:0,b:-2,n:2},
-		 ],
+	    conf:null
 	}
     },
-    mounted(){
-	let L=this.level;
-	for(let a=-L;a<=L;a++){
-	    for(let b=-L;b<=L;b++){
-		if(a+b>=-L && a+b<=L){
-		    this.coords.push({a:a,b:b});
-		}
-	    }
-	}
+    mounted(){//初期化の時だけ
+	this.conf=this.problem
+	console.log("Mounted")
+	console.log(this.problem)
     },
-    computed:{
+    computed:{//算出プロパティ
     },
     methods:{
+	coords: function(){
+	    let grid=[];
+	    let L=this.level;
+	    for(let a=-L;a<=L;a++){
+		for(let b=-L;b<=L;b++){
+		    if(a+b>=-L && a+b<=L){
+			grid.push({a:a,b:b});
+		    }
+		}
+	    }
+	    //console.log(grid);
+	    return grid;
+	},
 	neighbors(){
 	    let isNeighbor=function(x,y){
 		if(x.a-y.a==1 && x.b-y.b==0){
@@ -81,7 +76,7 @@ export default{
 	    
 	},
 	puton(e){
-	    console.log("puton"+" "+e.a+","+e.b+","+e.index);
+	    //console.log("puton"+" "+e.a+","+e.b+","+e.index);
 	    this.conf.splice(e.index,1,{a:this.conf[e.index].a+e.a, b:this.conf[e.index].b+e.b, n:this.conf[e.index].n})
 	    //splice(追加・削除する位置,削除する要素数,追加する要素),削除された要素が配列型式で返される			
 	    //console.log(e);
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-<svg width="800" height="800">
-    <board level="2" exp="50"></board>
-</svg>
+<div>
+  level:
+  <v-btn @click="minus()">-</v-btn>
+  {{level}}
+  <v-btn @click="plus()">+</v-btn>
+  <svg width="800" height="800">
+    <board :level="level" exp="50" :problem="problemset[0][0]"></board>
+  </svg>
+</div>
 
 </template>
 
@@ -9,8 +15,45 @@
 
 import board from '~/components/board.vue'
 export default {
+    data(){
+	return{
+	    level:2,
+	    problemset:[
+		[
+		    [{a:0,b:0,n:0}],	    
+		],
+		[
+		    [],
+		],
+		[
+		    [{a:0,b:1,n:2},
+			  {a:1,b:0,n:2},//
+			  {a:-1,b:1,n:2},//
+			  {a:1,b:-1,n:2},
+			  {a:-1,b:0,n:2},
+			  {a:0,b:-1,n:2},
+			  {a:-2,b:0,n:2},
+			  {a:-2,b:2,n:2},
+			  {a:0,b:2,n:2},//
+			  {a:2,b:0,n:2},
+			  {a:2,b:-2,n:2},
+			  {a:0,b:-2,n:2},
+			 ],],
+			[],
+			[]],
+	    
+	}
+    },
     components: {
 	board
+    },
+    methods:{
+	minus(){
+	    this.level--;
+	},
+	plus(){
+	    this.level++;
+	}
     }
 }
 </script>
